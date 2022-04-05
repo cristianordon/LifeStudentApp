3ae9c77438c36881fcf8c514448701b3dd0d8881
Cristiano Rodrigues de Oliveira Neto <crdon@aluno.ifnmg.edu.br>
2022-04-05 14:59:48 -0300

    atualizando - parte 4 concluída

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 
-import { Container, Title } from './styles';
+
+import {
+    Container,
+    Header,
+    ContentWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+
+} from './styles';
 
 export function Home() {
     return (
         <Container>
-            <Title>Life Student</Title>
-        </Container>
+            <Header>
+                <ContentWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://github.com/cristianordon.png' }} />
+                        <User>
+                            <UserGreeting>Olá, Usuário! </UserGreeting>
+
+
+                        </User>
+                    </UserInfo>
+                </ContentWrapper>
+            </Header>
+
+        </Container >
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,38 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
+   
+`;
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFValue(200)}px;
+    background-color:  ${({ theme }) => theme.colors.primary};
     align-items: center;
     justify-content: center;
+
 `;
-export const Title = styled.Text`
-    font-family: ${({ theme }) => theme.fonts.regular};
-    font-size: 23px;
-    color:  ${({ theme }) => theme.colors.title};
+
+export const ContentWrapper = styled.View``;
+
+export const UserInfo = styled.View``;
+
+export const Photo = styled.Image`
+    width: ${RFValue(100)}px;
+    height: ${RFValue(100)}px;
+    border-radius: 50px;
+`;
+
+export const User = styled.View`
+    margin: ${RFValue(12)}px`;
+
+
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.title};
+    font-family: ${({ theme }) => theme.fonts.regular_italic_mono};
+    font-size: ${RFValue(24)}px;
+    font-style: italic;
 `;
